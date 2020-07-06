989fbdf504ecf8277d84cf7ad5a5f66ed689f874
NoFoolLikeOne <nofool@paparomeo.uk>
2020-07-06 17:12:42 +0000

    Adding in counts by broad type.

--- biology.py
+++ biology.py
@@ -1,4 +1,8 @@
 #!/bin/env python3
+import pymysql
+from os import getenv
+from pymysql.err import OperationalError
+import httplib2
 
 import httplib2
 import os
@@ -9,12 +13,96 @@ from google.oauth2 import service_account
 from pathlib import Path
 from datetime import datetime
 
-HOME = str(Path.home())
 
 CAPI="https://api.canonn.tech"
 
 GRAPHQL="https://api.canonn.tech/graphql"
 
+def get_db_secrets():
+    dir=os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.join(dir,'canonn_db_secret.json')) as json_file:
+       data = json.load(json_file)
+       return data
+
+
+secret=get_db_secrets()
+
+mysql_conn= pymysql.connect(host=secret.get("DB_HOST"),
+                             user=secret.get("DB_USER"),
+                             password=secret.get("DB_PASSWORD"),
+                             db=secret.get("DB_NAME"))
+
+
+
+def __get_cursor():
+    """
+    Helper function to get a cursor
+      PyMySQL does NOT automatically reconnect,
+      so we must reconnect explicitly using ping()
+    """
+    try:
+        return mysql_conn.cursor()
+    except OperationalError:
+        mysql_conn.ping(reconnect=True)
+        return mysql_conn.cursor()
+
+def get_bio_signals():
+    system="Merope"
+
+    cursor =  mysql_conn.cursor(pymysql.cursors.DictCursor)
+    sql = """
+    select system,replace(body,system,'') as body,count from SAASignals where type = '$SAA_SignalType_Biological;';
+       """
+    cursor.execute(sql, ())
+    return cursor.fetchall()
+    cursor.clos()
+
+
+def journal_exists(jlist,journal_id):
+    if journal_id in list(jlist.keys()):
+        return True
+    else:
+        return False
+
+
+# we will have a global variable that we will update 
+bio_systems={}
+
+def populate_bio_systems(system,body,journal_id,journal_name,frontier_id,x,y,z,site_type):
+    if not frontier_id or frontier_id == '':
+        site_id="None"
+    else:
+        site_id=frontier_id
+
+    if not bio_systems.get(system):
+        bio_systems[system]={}
+    bio_systems[system]["coords"]=[x,y,z]
+    if not  bio_systems.get(system).get("body"):
+        bio_systems[system]["body"]={}
+    if not  bio_systems.get(system).get("body").get(body):
+        bio_systems[system]["body"][body]={}
+    if not  bio_systems.get(system).get("body").get(body).get("journal"):
+        bio_systems[system]["body"][body]["journal"]={}
+    if not  bio_systems.get(system).get("body").get(body).get("journal").get(journal_id):
+        bio_systems[system]["body"][body]["journal"][journal_id]={}
+
+    # if no site id we should either delete the key if a journal entry with a site id exists
+    have_journal=journal_exists(bio_systems[system]["body"][body]["journal"],journal_id)
+    no_site=(site_id == "None")
+    have_site=(site_id != "None")
+    journal_none=bio_systems.get(system).get("body").get(body).get("journal").get(journal_id).get("None")
+
+    if have_site and journal_none:
+        del bio_systems[system]["body"][body]["journal"][journal_id]["None"]
+
+    if have_journal:
+        bio_systems[system]["body"][body]["journal"][journal_id][site_id]={ "type": site_type, "name": journal_name}
+    else:
+        bio_systems[system]["body"][body]["journal"][journal_id]={}
+        bio_systems[system]["body"][body]["journal"][journal_id][site_id]={ "type": site_type, "name": journal_name}
+    #if bio_systems[system]["body"][body].get("site_type"):
+    #    bio_systems[system]["body"][body]["site_type"]={}
+    #bio_systems[system]["body"][body]["site_type"][site_type]=True
 
 
 def fetch_regions():
@@ -101,12 +189,29 @@ def get_sites(site):
     capi=fetch_capi(site)
     for row in capi:
        #print(row)
+       system=row.get("system").get("systemName")
+       body=str(row.get("body").get("bodyName").replace(row.get("system").get("systemName"),''))
+       journal_name=row.get("type").get("journalName")
+       journal_id=row.get("type").get("journalID")
+       x=row.get("system").get("edsmCoordX")
+       y=row.get("system").get("edsmCoordY")
+       z=row.get("system").get("edsmCoordZ")
+       #belt and braces in case strapi is not consistant
+       if row.get("frontierID"):
+           frontier_id=row.get("frontierID")
+       elif row.get("frontierId"):
+           frontier_id=row.get("frontierId")
+       else:
+           frontier_id = None
+       
+       populate_bio_systems(system,body,journal_id,journal_name,frontier_id,x,y,z,site)
+
        cols=[]
        cols.append(row.get("id"))
        cols.append(row.get("type").get("type"))
-       cols.append(row.get("type").get("journalName"))
-       cols.append(row.get("type").get("journalID"))
-       cols.append(row.get("system").get("systemName"))
+       cols.append(journal_name)
+       cols.append(journal_id)
+       cols.append(system)
        cols.append(row.get("system").get("edsmCoordX"))
        cols.append(row.get("system").get("edsmCoordY"))
        cols.append(row.get("system").get("edsmCoordZ"))
@@ -117,8 +222,8 @@ def get_sites(site):
            region="Unknown"
        cols.append(region)
        cols.append(row.get("system").get("primaryStar").get("type"))
-       cols.append(str(row.get("body").get("bodyName").replace(row.get("system").get("systemName"),'')))
-       cols.append(row.get("frontierID"))
+       cols.append(body)
+       cols.append(frontier_id)
        cols.append(row.get("body").get("subType"))
        cols.append(row.get("body").get("gravity"))
        cols.append(row.get("body").get("distanceToArrival"))
@@ -129,6 +234,8 @@ def get_sites(site):
        cols.append(row.get("body").get("orbitalEccentricity"))
        cols.append(row.get("discoveredBy").get("cmdrName"))
        retval.append(cols)
+
+       
        #print(json.dumps(col,indent=4))
     return retval
 
@@ -161,7 +268,7 @@ def write_sheet(spreadsheet_id,range_name,cells):
     print(f"Writing to sheet {name}")
     try:
         scopes = ["https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/spreadsheets"]
-        secret_file = os.path.join(HOME,'sheets', 'client_secret.json')
+        secret_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),'client_secret.json')
 
 
         credentials = service_account.Credentials.from_service_account_file(secret_file, scopes=scopes)
@@ -180,19 +287,104 @@ def write_sheet(spreadsheet_id,range_name,cells):
     except OSError as e:
         print(e)
 
+def count_sites(bodydata):
+    count=0
+    for journal,journaldata in bodydata.get("journal").items():
+        for site in journaldata.keys():
+            count+=1
+    return count
+
+def get_type_array(bodydata):
+    retval=[0,0,0,0,0]
+    for journal,journaldata in bodydata.get("journal").items():
+        for site,sitedata in journaldata.items():
+            for key,val in enumerate(["apsites","fgsites","btsites","cssites","twsites"]):
+                if sitedata.get("type") == val:
+                   retval[key]+=1
+    return retval
+
+
+def summarise_biosystems():
+    for system,systemdata in bio_systems.items():
+        #print(system, '->' ,systemdata )
+        for body,bodydata in systemdata.get("body").items():
+            bio_systems[system]["body"][body]["codex_sites"]=count_sites(bodydata)
+            bio_systems[system]["body"][body]["type_counts"]=get_type_array(bodydata)
+            #print(bio_systems[system]["body"][body]["type_counts"])
+
+def get_signal_cells():
+    for signal in bio_signals:
+        has_system=(bio_systems.get(signal.get("system")))
+        has_bodies=(has_system and bio_systems.get(signal.get("system")).get("body"))
+        has_body=(has_bodies and bio_systems.get(signal.get("system")).get("body").get(signal.get("body")))
+        system=signal.get('system')
+        if has_body:
+            #print(f"XXX BODY EXISTS {system}")
+            bio_systems[system]["body"][signal.get("body")]["signals"]=signal.get("count")
+            bio_systems[system]["body"][signal.get("body")]["has_body"]=True
+        elif has_bodies:
+            #print(f"XXX SOMEBODY EXISTS {system}")
+            bio_systems[system]["body"][signal.get("body")]={ "has_bodies": True, "signals": signal.get("count"), "codex_sites": 0}
+        elif has_system:
+            #print(f"XXX SYSTEM EXISTS {system}")
+            bio_systems[signal.get("system")]["body"]={}
+            bio_systems[signal.get("system")]["body"][signal.get("body")]={ "has_system": True,"signals": signal.get("count"), "codex_sites": 0}
+        else:
+            #print(f"XXX ORPHAN {system}")
+            bio_systems[signal.get("system")]={}
+            bio_systems[signal.get("system")]["body"]={}
+            bio_systems[signal.get("system")]["body"][signal.get("body")]={ "orphan": True, "signals": signal.get("count"), "codex_sites": 0}
+
+    signalcells=[]
+
+
+    for system,systemdata in bio_systems.items():
+        for body,bodydata in systemdata.get("body").items():
+            codex_sites=bodydata.get("codex_sites")
+            type_counts=bodydata.get("type_counts")
+            
+            if bodydata.get("signals"):
+                signals=bodydata.get("signals")
+            else:
+                signals="-"
+            row=[system,body,codex_sites,signals]
+            if type_counts:
+                row.extend(type_counts)
+            else:
+                row.extend([0,0,0,0,0])
+
+            signalcells.append(row)
+
+    return signalcells
+
+
+
 SHEETID="15lqZtqJk7B2qUV5Jb4tlnst6i1B7pXlAUzQnacX64Kc"
 GUARDIAN_SHEET="1p20iT3HWAcRRJ8Cw60Z2tCVTpcBavhycvE0Jgg0h32Y"
 CLOUD_SHEET="11BCZRci0YlgW0sFdxvB_srq7ssxHzstMAiewhSGHE94"
 
 cells=[]
 cells.append([str(datetime.now().isoformat(timespec='minutes'))])
-write_sheet(SHEETID,'Crystaline Shard Sites!A1:Z',get_sites("cssites"))
-write_sheet(SHEETID,'Anemone Sites!A1:Z',get_sites("fgsites"))
 write_sheet(SHEETID,'Amphora Plant Sites!A1:Z',get_sites("apsites"))
+
+
+write_sheet(SHEETID,'Anemone Sites!A1:Z',get_sites("fgsites"))
+write_sheet(SHEETID,'Crystaline Shard Sites!A1:Z',get_sites("cssites"))
 write_sheet(SHEETID,'Sinuous Tuber Sites!A1:Z',get_sites("twsites"))
 btcells=get_sites("btsites")
 write_sheet(SHEETID,'Brain Tree Sites!A1:Z',btcells)
 write_sheet(GUARDIAN_SHEET,'Brain Tree Sites!A1:Z',btcells)
+
+
 write_sheet(CLOUD_SHEET,'Lagrange Cloud Reports!A1:Z',get_lc())
 write_sheet(SHEETID,'Header!B3',cells)
 write_sheet(CLOUD_SHEET,'Header!B3',cells)
+
+#summarise the biosytems
+summarise_biosystems()
+bio_signals=get_bio_signals()
+
+write_sheet(SHEETID,'Signals!A2:Z',get_signal_cells())
+
+
+
