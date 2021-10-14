150ac8b12cfcbf5e37d799ed2982aab7dc95af7b
NoFoolLikeOne <nofool@paparomeo.uk>
2021-10-14 08:11:31 +0000

    Adding platform to stats

--- spansh.py
+++ spansh.py
@@ -68,7 +68,7 @@ def __get_cursor():
 def get_codex_data():
     cursor = mysql_conn.cursor(pymysql.cursors.DictCursor)
     sql = """
-        SELECT cr.name,cast(reported_at as char) as reported_at,system,body,cr.entryid,english_name,sub_class,IFNULL(id64 ,raw_json->"$.SystemAddress") AS systemaddress,cmdrname FROM codexreport cr
+        SELECT cr.name,cast(reported_at as char) as reported_at,system,body,cr.entryid,english_name,sub_class,IFNULL(id64 ,raw_json->"$.SystemAddress") AS systemaddress,cmdrname,platform FROM codexreport cr
         LEFT JOIN codex_name_ref cnr ON cnr.entryid = cr.entryid
         WHERE body IS NOT NULL AND body != '' AND (hud_category = 'Biology' or english_name like '%%Barnacle%%')
         ORDER BY created_at asc
@@ -227,6 +227,7 @@ def initStats(codex, grav, temp, atmo, bodytype, star, parentstar, pressure, sol
     biostats[codex.get("entryid")] = {
         "name": codex.get("english_name"),
         "id": codex.get("name"),
+        "platform": codex.get("platform"),
         "bodies": set([bodytype]),
         "ming": refloat(grav),
         "maxg": refloat(grav),
